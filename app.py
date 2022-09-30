f543aaf5ab6b696fa2d99a865ec1e457a1e36f89
kaljo14 <kaloyan.i.2000@icloud.com>
2022-09-30 21:19:12 +0300

    connect to DB

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
+from ast import dump
+from crypt import methods
+from pickle import TRUE
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import pymysql
+from pprint import pprint
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/barber_shop'
+db = SQLAlchemy(app)
 
 
-@app.route('/')
+class barbers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    description = db.Column(db.String())
+    image = db.Column(db.String())
+    status = db.Column(db.String())
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<barbers:{self.name}>'
+
+
+@app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+
+    barber = barbers.query.all()
+    pprint(globals(barber))
+
+    return render_template('index.html', barber=barber)
 
 
 if __name__ == "__main__":
