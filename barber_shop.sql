519e90bdf51da1ba82dff9692fbf7a689dfb81f1
kaljo14 <kaloyan.i.2000@icloud.com>
2022-10-04 20:31:40 +0300

    barber

---
+++ barber_shop.sql
@@ -0,0 +1,293 @@
+-- MySQL dump 10.13  Distrib 8.0.30, for macos12.4 (arm64)
+--
+-- Host: localhost    Database: barber_shop
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `barbers`
+--
+
+DROP TABLE IF EXISTS `barbers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `barbers` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'avaliable',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `barbers`
+--
+
+LOCK TABLES `barbers` WRITE;
+/*!40000 ALTER TABLE `barbers` DISABLE KEYS */;
+INSERT INTO `barbers` VALUES (2,'ivan petrov','i am not so good','public/barbers/NklPGMzvlnMOuVzL4Y0TZ7UtqI2OHOV5k9jOHsHe.jpg','avaliable','2022-06-22 11:27:26','2022-06-22 11:27:26'),(3,'Viki','she is bad','public/barbers/R6HVMTzp47yiUdgXkjQXpIGJbTXak6eViu6lEN9n.png','avaliable','2022-09-22 03:54:29','2022-09-22 03:54:29'),(4,'Pierre Varona','hj','public/barbers/8O3gFgUVAQhPhUXo44iiOGPgH2EpzHNXlFtlcEki.png','avaliable','2022-09-28 08:05:14','2022-09-28 08:05:14');
+/*!40000 ALTER TABLE `barbers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categories` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `price` decimal(8,2) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (1,'Beard Trim','Based on your hair growth and face shape, we will choose the type of beard that will suit you best. You will be given tips on how to maintain your beard yourself.','public/categories/vJPwG2TZxY9FFGw15yPC4Sul8oAj4VFMiu8Qztj8.jpg',15.00,'2022-06-22 11:25:42','2022-06-22 12:58:03'),(2,'Hair cut','First you will consult a professional about your hair type. Then - we will wash, cut and style your hair with products to achieve the desired result.','public/categories/sUR4SDn9BSNWuq6urwtwyq9ke9CQSy9exVFfTHqw.jpg',25.00,'2022-06-22 11:26:12','2022-06-22 12:57:38'),(3,'Combo','We offer to combine your haircut and beard shaping in one visit.This is our budget option.Nevertheless we offer the same care and attention.','public/categories/AQmjks6wNXW7Pm2HLelbtB0uUXSQ68aIg5bB5mXx.jpg',35.00,'2022-06-22 13:01:18','2022-06-22 13:05:45');
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `failed_jobs`
+--
+
+DROP TABLE IF EXISTS `failed_jobs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `failed_jobs` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `failed_jobs`
+--
+
+LOCK TABLES `failed_jobs` WRITE;
+/*!40000 ALTER TABLE `failed_jobs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `failed_jobs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `migrations`
+--
+
+DROP TABLE IF EXISTS `migrations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `migrations` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `migrations`
+--
+
+LOCK TABLES `migrations` WRITE;
+/*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
+INSERT INTO `migrations` VALUES (1,'2014_10_12_000000_create_users_table',1),(2,'2014_10_12_100000_create_password_resets_table',1),(3,'2019_08_19_000000_create_failed_jobs_table',1),(4,'2019_12_14_000001_create_personal_access_tokens_table',1),(5,'2022_06_12_100141_create_categories_table',1),(6,'2022_06_12_100247_create_barbers_table',1),(7,'2022_06_12_100329_create_reservations_table',1),(11,'2022_09_22_194326_create_reports_table',2);
+/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `password_resets`
+--
+
+DROP TABLE IF EXISTS `password_resets`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  KEY `password_resets_email_index` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `password_resets`
+--
+
+LOCK TABLES `password_resets` WRITE;
+/*!40000 ALTER TABLE `password_resets` DISABLE KEYS */;
+/*!40000 ALTER TABLE `password_resets` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+
+DROP TABLE IF EXISTS `personal_access_tokens`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenable_id` bigint unsigned NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abilities` text COLLATE utf8mb4_unicode_ci,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `personal_access_tokens`
+--
+
+LOCK TABLES `personal_access_tokens` WRITE;
+/*!40000 ALTER TABLE `personal_access_tokens` DISABLE KEYS */;
+/*!40000 ALTER TABLE `personal_access_tokens` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reports`
+--
+
+DROP TABLE IF EXISTS `reports`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reports` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `salary` decimal(8,2) NOT NULL,
+  `month` date NOT NULL,
+  `barber_id` bigint unsigned NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `reports_barber_id_foreign` (`barber_id`),
+  CONSTRAINT `reports_barber_id_foreign` FOREIGN KEY (`barber_id`) REFERENCES `barbers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reports`
+--
+
+LOCK TABLES `reports` WRITE;
+/*!40000 ALTER TABLE `reports` DISABLE KEYS */;
+INSERT INTO `reports` VALUES (17,325.00,'2022-09-28',2,'2022-09-28 08:35:15','2022-09-28 08:35:15'),(18,50.00,'2022-09-28',3,'2022-09-28 08:35:15','2022-09-28 08:35:15'),(19,0.00,'2022-09-28',4,'2022-09-28 08:35:15','2022-09-28 08:35:15'),(20,325.00,'2022-09-28',2,'2022-09-28 08:35:38','2022-09-28 08:35:38'),(21,50.00,'2022-09-28',3,'2022-09-28 08:35:38','2022-09-28 08:35:38'),(22,0.00,'2022-09-28',4,'2022-09-28 08:35:38','2022-09-28 08:35:38');
+/*!40000 ALTER TABLE `reports` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reservations`
+--
+
+DROP TABLE IF EXISTS `reservations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reservations` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone_number` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `reser_date` datetime NOT NULL,
+  `barber_id` bigint unsigned NOT NULL,
+  `category_id` bigint unsigned NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `reservations_barber_id_foreign` (`barber_id`),
+  KEY `reservations_category_id_foreign` (`category_id`),
+  CONSTRAINT `reservations_barber_id_foreign` FOREIGN KEY (`barber_id`) REFERENCES `barbers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `reservations_category_id_foreign` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reservations`
+--
+
+LOCK TABLES `reservations` WRITE;
+/*!40000 ALTER TABLE `reservations` DISABLE KEYS */;
+INSERT INTO `reservations` VALUES (9,'kaloyan','ivanov','vvladimirova@hawk.iit.edu','0888012463','2022-06-26 15:00:00',2,3,'2022-06-24 18:30:15','2022-06-24 18:30:15'),(11,'kaloyan','ivanov','ivko_iv_260399@abv.bg','0888012463','2022-07-05 17:00:00',2,1,'2022-06-25 13:48:53','2022-06-25 13:48:53'),(14,'kaloyan','ivanov','ivko_iv_260399@abv.bg','0888012463','2022-07-11 16:00:00',2,2,'2022-06-25 14:14:48','2022-06-25 14:14:48'),(19,'kaloyan','ivanov','ivko_iv_260399@abv.bg','0888012463','2022-07-12 14:00:00',2,2,'2022-06-25 16:25:22','2022-06-25 16:25:22'),(21,'kaloyan','ivanov','vvladimirova@hawk.iit.edu','0888012463','2022-07-20 18:00:00',2,3,'2022-06-25 16:30:48','2022-06-25 16:30:48'),(22,'kaloyan','ivanov','ivko_iv_260399@abv.bg','0888012463','2022-07-12 12:00:00',3,2,'2022-06-25 16:33:32','2022-06-25 16:33:32'),(24,'kaloyan','ivanov','ivko_iv_260399@abv.bg','0888012463','2022-07-03 13:00:00',2,3,'2022-06-25 16:38:18','2022-06-25 16:38:18'),(26,'kaloyan','ivanov','vvladimirova@hawk.iit.edu','0888012463','2022-07-14 14:00:00',3,2,'2022-06-25 16:53:53','2022-06-25 16:53:53'),(27,'kalo','ivan','imai.ex@gmail.com','0128821312','2022-07-01 11:00:00',2,2,'2022-06-26 16:13:38','2022-06-26 16:13:38'),(28,'kalo','petrov','kaloyan.i.2000@abv.bg','0128821312','2022-07-01 12:00:00',2,2,'2022-06-26 16:55:11','2022-06-26 16:55:11'),(29,'ivan','ivan','imai.ex@gmail.com','0128821312','2022-07-12 15:00:00',2,2,'2022-06-26 17:05:01','2022-06-26 17:05:01'),(30,'ivan','ivan','imai.ex@gmail.com','0128821312','2022-07-12 15:00:00',2,2,'2022-06-26 17:08:08','2022-06-26 17:08:08'),(31,'ivan','ivan','imai.ex@gmail.com','0128821312','2022-07-12 15:00:00',2,2,'2022-06-26 17:09:12','2022-06-26 17:09:12'),(34,'kaloyan','ivanov','vvladimirova@hawk.iit.edu','0888012463','2022-09-08 12:00:00',2,1,'2022-09-09 00:42:56','2022-09-09 00:42:56'),(35,'kaloyan','ivanov','k.desislavov@abv.bg','0888012463','2022-09-12 14:00:00',2,1,'2022-09-09 00:50:39','2022-09-09 00:50:39');
+/*!40000 ALTER TABLE `reservations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `is_admin` tinyint(1) NOT NULL DEFAULT '0',
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Admin','k.desislavov@abv.bg','2022-06-21 16:41:50','$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',1,'qRPuQh5ZQqGnhYPeTSKlRI2KKAL2RJsaaNCTse8tuGPpv3s6d4zVWvOQR07g','2022-06-21 16:41:50','2022-06-21 16:41:50');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-04 20:26:05
